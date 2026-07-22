f9d860c98af7287921742a8aff5add0a1eec1e79
angelodiazz <angelodiazm10@gmail.com>
2026-07-22 08:39:19 -0400

    chore: align payroll documentation and CLI build

---
+++ PayrollSystemQt1/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_PayrollSystemQt_FILE C:/Users/angel/OneDrive/Qt/PayrollSystemQt1/PayrollSystemQt1/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/PayrollSystemQt.exe)
+set(__QT_DEPLOY_TARGET_PayrollSystemQt_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_PayrollSystemQt_RUNTIME_DLLS C:/Qt/6.10.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
